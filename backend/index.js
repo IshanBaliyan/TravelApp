0dccda8e99bc11c8662eab7eec86c90aa2d72a2f
Ishan <ishanbaliyan@gmail.com>
2021-08-11 22:38:34 -0400

    Changed cor code

--- backend/index.js
+++ backend/index.js
@@ -25,13 +25,37 @@ app.use(express.json())
 //          useNewUrlParser: true, 
 //          useUnifiedTopology: true 
 //     })
-const cors = require('cors');
-app.use(cors({credentials : true}));
-app.use(function (req, res, next) {	
-    res.setHeader('Access-Control-Allow-Origin', '*');    
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
-    res.setHeader('Access-Control-Allow-Credentials', true);    
+// const cors = require('cors');
+// app.use(cors({credentials : true}));
+// app.use(function (req, res, next) {	
+//     res.setHeader('Access-Control-Allow-Origin', '*');    
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
+//     res.setHeader('Access-Control-Allow-Credentials', true);    
+//     next();
+// });
+
+var cors = require('cors');
+
+// use it before all route definitions
+app.use(cors({origin: 'https://travel-pin-app.netlify.app/'}));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://travel-pin-app.netlify.app/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
 });
 
